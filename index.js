39577f685d580b757123107162c32d9131ff7f20
moca00black <78536724+moca00black@users.noreply.github.com>
2022-07-29 19:02:54 +0900

    Try implementing mongoose

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express')
 const cowsay = require('cowsay')
 const cors = require('cors')
 const path = require('path')
+const mongoose = require("mongoose");
+const Category = require("../models/Category");
 
 // Create the server
 const app = express()
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
 
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
@@ -23,8 +33,10 @@ app.get('/api/cow/:say', cors(), async (req, res, next) => {
 // Serve our base route that returns a Hellow World cow
 app.get('/api/cow/', cors(), async (req, res, next) => {
   try {
-    const moo = cowsay.say({ text: 'Hello World!' })
-    res.json({ moo })
+    const cats = await Category.find();
+
+    const moo = cowsay.say({ text: cats })
+    res.json(cats)
   } catch (err) {
     next(err)
   }
